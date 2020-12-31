d3fc07ff9051fa9560d39ed3d649a8d4bca64a91
saifbashar <saifbashar2021@gmail.com>
2020-12-31 22:11:54 +0600

    t

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var myName = prompt("What is your name:");
-if(myName == 'depaneeta tisha' || myName=="Depaneeta" || myName == "tisha")
+if(myName == "depaneeta tisha" || myName=="Depaneeta" || myName == "tisha")
 {
     alert("You are a chagol");
 }
